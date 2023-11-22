bba0248e7a196f446ae7800ff773047b080ed9c5
sreephaneesha <sreephaneesha2005@gmail.com>
2023-11-22 20:20:40 +0530

    Update script.js

--- 54 Introduction to JavaScript & Installing Node.js/script.js
+++ 54 Introduction to JavaScript & Installing Node.js/script.js
@@ -4,6 +4,7 @@ console.log("Code is running...")
 console.log("Code is also running...")
 console.log("Code is looking like a wow...")
 
+console.warn("warning ")
 var a = prompt("Enter your number")
 var isTrue = confirm("Are you sure you want to leave this page and blast your computer ")
 
