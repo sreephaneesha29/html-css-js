bd57f044a72d25c83032222e8703798d8d16388c
sreephaneesha <sreephaneesha2005@gmail.com>
2023-11-25 18:11:10 +0530

    Create index.js

---
+++ 60 JavaScript Strings/index.js
@@ -0,0 +1,34 @@
+console.log("strings tutorial")
+let a="sreephaneesh"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+console.log(a[8])
+console.log(a[9])
+console.log(a[10])
+console.log(a[11])
+console.log(a.length)
+let realname="sreephaneesh"
+let friend="harry"
+console.log("his name is " + realname + " and his friend name is " + friend) 
+
+console.log(`his name is ${realname} and his friend name is ${friend}`)
+console.log("alen\"gth")
+console.log("allen\ngth")
+let b="shiva"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+console.log(b.replace("sh","66"))
+console.log(b.concat(a,"GUI"))
+console.log(b.endsWith("a"))
+console.log(b.startsWith("sh"))
+console.log(b.charAt("3"))
+console.log(b.indexOf("i"))
