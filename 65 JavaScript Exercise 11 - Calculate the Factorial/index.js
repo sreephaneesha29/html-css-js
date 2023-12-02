34a4babef1a88e5d41e8c804b48e3acb4183266e
sreephaneesha <150938117+sreephaneesha29@users.noreply.github.com>
2023-12-02 03:24:15 +0000

    65

---
+++ 65 JavaScript Exercise 11 - Calculate the Factorial/index.js
@@ -0,0 +1,9 @@
+
+//facotrial using for loop
+num=5
+product=1
+for (let index = num; index > 0; index--) {
+    product=product*index
+    
+}
+console.log(product)
--- README.md
+++
@@ -1,8 +0,0 @@
-# html-css-js
-Source Code For HTML CSS JS 
-<br>
-Author - @sreephaneesha29
-<br>
-replit.com/@sreephaneesha29
-<br>
-# sigma web dev 
