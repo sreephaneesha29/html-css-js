315369300cf8dee47771fec559b0547211b675f2
sreephaneesha <sreephaneesha2005@gmail.com>
2023-12-05 19:23:07 +0530

    67

--- 63 JavaScript Arrays/map_filter_reduce.js
+++ 63 JavaScript Arrays/map_filter_reduce.js
@@ -18,4 +18,10 @@ const greaterThanSeven = (e)=>{
 }
 console.log(arr.filter(greaterThanSeven))
 
- 
+let arr2 = [1,2,3,4,5,6]
+
+const red = (a, b)=>{
+    return a+b
+}
+
+console.log(arr2.reduce(red))
